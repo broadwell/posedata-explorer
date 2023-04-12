2ff4a551c269149cbbbcddd8c31ee1fe49f87682
Simon Wiles <simonjwiles@gmail.com>
2023-04-12 16:22:34 -0700

    Bootstrap build environment before attempting to install from `Pipfile`

--- start_notebook_server.sh
+++ start_notebook_server.sh
@@ -10,7 +10,8 @@ fi
 KERNEL_NAME=${KERNEL_NAME:-posedata-explorer}
 
 # install/update virtualenv as needed
-PIPENV_VENV_IN_PROJECT=1 python -m pipenv install
+PIPENV_VENV_IN_PROJECT=1 python -m pipenv run pip install cython numpy
+python -m pipenv install
 
 # install/update an ipykernel based on this virtualenv
 pipenv run python -m ipykernel install --user --name="$KERNEL_NAME"
